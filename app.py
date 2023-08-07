e9a8b6a6ed50b8d99fc5cd40d787375599138d18
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-08-07 15:54:52 +0500

    Add basic DB operations

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+import sqlite3
+
+from flask import Flask, render_template, g
+
 
 app = Flask(__name__)
+
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect("database.db", detect_types=sqlite3.PARSE_DECLTYPES)
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db():
+    if (db := g.pop("db", None)) is not None:
+        db.close()
 
 
 @app.get("/")
