e01ac5bd7bf3fdf0ba3e5d29db60db466afcf029
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-08-08 16:47:48 +0500

    Move DB logic to separate file & refactor it

--- app.py
+++ app.py
@@ -1,29 +1,12 @@
-import sqlite3
+from flask import Flask, render_template
 
-from flask import Flask, render_template, g
+from database import get_index_view_data
 
 
 app = Flask(__name__)
 
 
-def get_db():
-    if "db" not in g:
-        g.db = sqlite3.connect("database.db", detect_types=sqlite3.PARSE_DECLTYPES)
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-
-def close_db():
-    if (db := g.pop("db", None)) is not None:
-        db.close()
-
-
 @app.get("/")
 def index():
-    db = get_db()
-    cur = db.cursor()
-    energy_drinks = cur.execute("SELECT brand, flavor, rating, description FROM energy_drinks").fetchall()
-    columns_headers = [key.capitalize() for key in energy_drinks[0].keys()]
-    cur.close()
+    columns_headers, energy_drinks = get_index_view_data()
     return render_template("index.html", energy_drinks=energy_drinks, columns_headers=columns_headers)
---
+++ database.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+from flask import g
+
+
+def _get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect("database.db", detect_types=sqlite3.PARSE_DECLTYPES)
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+
+def _close_db():
+    if (db := g.pop("db", None)) is not None:
+        db.close()
+
+
+def get_index_view_data():
+    with (con := _get_db()):
+        energy_drinks = con.execute("SELECT brand, flavor, rating, description FROM energy_drinks").fetchall()
+    columns_headers = [key.capitalize() for key in energy_drinks[0].keys()]
+    return columns_headers, energy_drinks
